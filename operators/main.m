740880dfa8d5a49490ad1ae72f5a2bcaff8a062b
nordbjorn <ksenia.ustrimova@gmail.com>
2016-09-06 22:30:01 +0200

    Initial Commit

---
+++ operators/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  operators
+//
+//  Created by Bjørn on 06/09/16.
+//  Copyright © 2016 Bjørn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
